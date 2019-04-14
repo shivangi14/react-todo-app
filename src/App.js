81de7e5adfea07520b6a3feab6f1546adaec67cd
shivangi14 <agshivangi14@gmail.com>
2019-04-14 23:01:17 +0530

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import Header from "./components/Header"
+
+function App(){
+    return(
+        <div>
+            <Header />
+            <input type="checkbox" /><span>market</span><br/>
+            <input type="checkbox" /><span>slippers</span><br/>
+            <input type="checkbox" /><span>watch</span>
+        </div>
+    )
 }
 
-export default App;
+export default App

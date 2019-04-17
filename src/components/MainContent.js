b5bd63bd79785dc6413529249e830f929b5cc49d
shivangi14 <agshivangi14@gmail.com>
2019-04-17 16:55:11 +0530

    class based component and state added

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,13 +2,19 @@ import React from "react"
 import ToDoItem from "./ToDoItem"
 import todosData from "./todosData"
 
-function MainContent(){
-    const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
-    return(
-        <div className = "toDoList">
-            {todoItems}
-        </div>
-    )
+class MainContent extends React.Component{
+    constructor(){
+        super()
+        this.state = {todoItems : []}
+    }
+    render(){
+        this.state.todoItems = todosData.map(item => <ToDoItem key={item.id} item={item} />)
+        return(
+                <div className = "toDoList">
+                    {this.state.todoItems}
+                </div>
+            )
+    }
 }
 
 export default MainContent

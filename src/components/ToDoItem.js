635ba98a7742b9a861a815636db8f610cc4b2ec5
shivangi14 <agshivangi14@gmail.com>
2019-04-17 23:46:05 +0530

    added event handling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from "react"
 function ToDoItem(props){
     return(
         <div className = "toDoItem">
-            <input type="checkbox" checked={props.item.completed}/>
+            <input type="checkbox" checked={props.item.completed} onChange={()=> console.log("I am done!")}/>
             <span style ={{textDecoration : props.item.completed ? "line-through" :"none"}}>{props.item.text}</span>
             <br/>
         </div>
